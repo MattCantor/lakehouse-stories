eba01e788a2d5b398e09a32aebc32e91a41b0a6d
Matthew Cantor <matthewcantor@gmail.com>
2023-09-05 12:14:41 -0400

    created midx-components.js in root

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withMDX = require('@next/mdx')({extension: /\.mdx?$/})
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        mdxRs: true,
+    },
+}
 
+const withMDX = require('@next/mdx')()
 module.exports = withMDX(nextConfig)
