0fa681dc898f137a3448d38a9733164c8a52aa17
Matthew Cantor <matthewcantor@gmail.com>
2023-09-08 19:26:22 -0400

    fixed bug, styling stories

--- src/app/chapter/[slug]/page.jsx
+++ src/app/chapter/[slug]/page.jsx
@@ -2,27 +2,32 @@ import { client } from '../../../../tina/__generated__/client.js'
 // import Chapter from '../../../components/chapter.jsx'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
 
+export const dynamic = "force-dynamic"
+
+const components = {
+  p: (props) => {
+    return (
+      <p className="indent-2 text-justify pb-2 first-of-type:first-letter:text-xl first-of-type:first-letter:font-bold first-of-type:first-letter:uppercase">{props.children}</p>
+    )
+  }
+}
+
 export default async function Page({ params }) {
 
   const { slug } = params
-  console.log(`slug: ${slug}`)
   const post = await client.queries.chapter({relativePath: `${slug}.mdx`})
-  console.log(post.data.chapter.body.children.map(children => children.children))
 
   return (<>
-      {/* <h1>{post.data.chapter.title}</h1>
-      <h2>{post.data.chapter.synopsis}</h2> */}
-      <TinaMarkdown content={post.data.chapter.body}/>
+      <h1 className="tracking-wide uppercase text-center font-bold">{post.data.chapter.title}</h1>
+      <h2 className="text-center italic pb-2">{post.data.chapter.synopsis}</h2>
+      <TinaMarkdown content={post.data.chapter.body} components={components}/>
     </>)
   }
 
   export async function generateStaticParams() {
 
     const data = await client.queries.chapterConnection()
-    // console.table(`data: ${data.data.chapterConnection.edges[0].node.body.children[0].children[0].text}`)
     const titleArray = data.data.chapterConnection.edges.map(edge => edge.node)
-    // console.log(`titleArray: ${titleArray}`)
-    // console.table(`nodeID: ${titleArray.map(node => ({id: node.id}))[0].id}`)
    
     return titleArray.map(node => ({
       id: node.id
