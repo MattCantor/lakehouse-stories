7e28495eb6c2bd3af779863a2c45d3731fc46d40
Matthew Cantor <matthewcantor@gmail.com>
2023-09-24 22:47:25 -0400

    testing removing background

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-center bg-no-repeat bg-cover bg-[url('/illustrations/lakehouse/Lakehouse1.1.png')] min-h-screen`}>
+      <body className={`${inter.className}`}>
           <Navbar/>
           {children}
       </body>
     </html>
   )
 }
+
+// bg-center bg-no-repeat bg-cover bg-[url('/illustrations/lakehouse/Lakehouse1.1.png')] min-h-screen
