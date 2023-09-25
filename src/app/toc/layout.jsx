39ad3a3f32b8b4abe81d697a4deba4c741fb5119
Matthew Cantor <matthewcantor@gmail.com>
2023-09-24 20:39:34 -0400

    debugging iOS issues

--- src/app/toc/layout.jsx
+++ src/app/toc/layout.jsx
@@ -9,13 +9,13 @@ export default function TOCLayout({
         <section
           className="container border-solid border-gray-200 shadow-page mt-6 mx-auto mb-0 pt-14 px-12 pb-12 max-w-2xl bg-white overflow-hidden"
         >
-          <Image
+          {/* <Image
             src={Cabin}
             alt="Lakehouse"
             fill={true}
             objectFit='cover'
             className='relative z-[-1] bg-blend-hue'
-          />
+          /> */}
           {children}
         </section>
       </main>
