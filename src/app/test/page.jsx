b341060cefa3b01b81ebab7a9894f135f83c21a1
Matthew Cantor <matthewcantor@gmail.com>
2023-09-24 23:21:07 -0400

    debugging iOS issues

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -3,7 +3,7 @@ export default function TestPage() {
       <main>
         <section className="container border-solid border-gray-200 shadow-page mt-5 mx-auto mb-0 pt-10 px-12 pb-12 max-w-2xl bg-white max-h-[90vh] overflow-y-auto">
           <div>
-          Mr. Finnigan and is responsible and well-respected member of the community. He would make a great mayor. Wally is an untrustworthy and dishonest predator. Everyone knows that Wally should not be the mayor. But, he's is _very_ well liked and very clever.
+          {"Mr. Finnigan and is responsible and well-respected member of the community. He would make a great mayor. Wally is an untrustworthy and dishonest predator. Everyone knows that Wally should not be the mayor. But, he's is _very_ well liked and very clever."}
           </div>
         </section>
       </main>
